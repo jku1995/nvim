a8b38394a6ace1e357b0994288c54ab30b7ce456
yef <1061380138@qq.com>
2022-05-21 17:17:46 +0800

    opt plugin.lua struct

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -4,39 +4,44 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function()
-  --vim中文手册
-  use 'yianwillis/vimcdoc'
-  -- Packer包管理器
-  use 'wbthomason/packer.nvim'
-  -- Nord主题
-  use 'shaunsingh/nord.nvim'
-  --当运行TSUpdate才加载
-  use {
-        'nvim-treesitter/nvim-treesitter',
-        run = ':TSUpdate'
-    }
-  --lualine 底部增加一个状态栏 
-  use {
-  'nvim-lualine/lualine.nvim',
-  requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+----------
+--基础----
+----------
+--vim中文手册
+use 'yianwillis/vimcdoc'
+--Packer包管理器
+use 'wbthomason/packer.nvim'
+--Nord主题
+use 'shaunsingh/nord.nvim'
+--当运行TSUpdate才加载
+use {
+      'nvim-treesitter/nvim-treesitter',
+      run = ':TSUpdate'
   }
-  --bufferline
-  use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
-  --nvim-tree
-  use {
-    'kyazdani42/nvim-tree.lua',
-    requires = {
-      'kyazdani42/nvim-web-devicons', -- optional, for file icon
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
-  --which key--
-  use {"folke/which-key.nvim",}
+--lualine 底部增加一个状态栏 
+use {
+'nvim-lualine/lualine.nvim',
+requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+}
+--bufferline
+use {'akinsho/bufferline.nvim',
+      tag = "v2.*",
+      requires = 'kyazdani42/nvim-web-devicons'}
+--nvim-tree
+use {
+  'kyazdani42/nvim-tree.lua',
+  requires = {
+    'kyazdani42/nvim-web-devicons', -- optional, for file icon
+  },
+  tag = 'nightly' -- optional, updated every week. (see issue #1193)
+}
+--which key--
+use {"folke/which-key.nvim",}
 
-  --telescope.nvim 望远镜
-  use {
-  'nvim-telescope/telescope.nvim',
-  requires = { {'nvim-lua/plenary.nvim'} }
+--telescope.nvim 望远镜
+use {
+'nvim-telescope/telescope.nvim',
+requires = { {'nvim-lua/plenary.nvim'} }
 }
 -----------
 ----cmp----

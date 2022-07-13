7183e599826fd6caeedfc655adb583a58b02c279
yef <1061380138@qq.com>
2022-07-13 22:48:49 +0800

    update nvim-tree setting to adopt new version

--- lua/lsp/init.lua
+++ lua/lsp/init.lua
@@ -1,3 +1,4 @@
 require("lsp.sumneko_lua")
 require("lsp.bashls")
 require("lsp.clangd")
+require("lsp.dockerls")
--- lua/nvim-tree-config/init.lua
+++ lua/nvim-tree-config/init.lua
@@ -1,24 +1,4 @@
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
-vim.g.nvim_tree_icons = {
-  default = "",
-  symlink = "",
-  git = {
-    unstaged = "",
-    staged = "S",
-    unmerged = "",
-    renamed = "➜",
-    deleted = "",
-    untracked = "U",
-    ignored = "◌",
-  },
-  folder = {
-    default = "",
-    open = "",
-    empty = "",
-    empty_open = "",
-    symlink = "",
-  },
-}
 require'nvim-tree'.setup {
     auto_reload_on_write = true,--写入自动刷新(默认）
     disable_netrw = true,--是否关闭自带文件管理器
@@ -32,6 +12,28 @@ require'nvim-tree'.setup {
         update_cwd = true,
       },
     renderer = {
+      icons = {
+        glyphs={
+          default = "",
+          symlink = "",
+          folder = {
+            default = "",
+            open = "",
+            empty = "",
+            empty_open = "",
+            symlink = "",
+          },
+          git={
+            unstaged = "",
+            staged = "S",
+            unmerged = "",
+            renamed = "➜",
+            deleted = "",
+            untracked = "U",
+            ignored = "◌",
+          },
+        },
+      },
       indent_markers = {
         enable = true,
         icons = {

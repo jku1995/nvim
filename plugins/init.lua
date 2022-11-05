4d30fce58a0b8116fff0677444eb1ef70a3dbeb5
yu <1061380138@qq.com>
2022-11-06 00:59:58 +0800

    fix nvim-tree拼写错误,add rust lsp支持,remove 不启用html和css lsp

--- plugins/init.lua
+++ plugins/init.lua
@@ -1,11 +1,17 @@
 return {
-  ["nvim-tree/nvim-tree.lua"] = {
+  ["nvim-tree/nvim-tree"] = {
     overide_options = {
        update_focused_file = {
         enable = true,
         update_root = true,
-        ignore_list = {},
       },
-    }
-  }
+    },
+  },
+  -- We are just modifying lspconfig's packer definition table
+["neovim/nvim-lspconfig"] = {
+    config = function()
+--      require "plugins.configs.lspconfig"
+      require "custom.plugins.lspconfig"
+    end,
+},
 }
---
+++ plugins/lspconfig/init.lua
@@ -0,0 +1,15 @@
+-- custom.plugins.lspconfig
+local on_attach = require("plugins.configs.lspconfig").on_attach
+local capabilities = require("plugins.configs.lspconfig").capabilities
+local present, lspconfig = pcall(require, "lspconfig")
+if not present then
+  return
+end
+local servers = {"clangd","rust_analyzer"}
+
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+  }
+end

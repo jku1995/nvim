efa66db2cc2de640f972fdf63a94633fa80eaccc
yu <1061380138@qq.com>
2022-11-05 23:36:32 +0800

    nvim-tree:调整目录树展示路径,跟随打开的文件

---
+++ chadrc.lua
@@ -0,0 +1,9 @@
+-- First read our docs (completely) then check the example_config repo
+
+local M = {}
+
+M.ui = {
+  theme = "nightfox",
+}
+M.plugins= require("custom.plugins")
+return M
---
+++ plugins/init.lua
@@ -0,0 +1,11 @@
+return {
+  ["nvim-tree/nvim-tree.lua"] = {
+    overide_options = {
+       update_focused_file = {
+        enable = true,
+        update_root = true,
+        ignore_list = {},
+      },
+    }
+  }
+}
